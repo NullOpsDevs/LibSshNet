0f7622ebb87120d51b2034b4cbf96395eddd6638
0x25CBFC4F <me@voids.site>
2025-11-02 00:27:34 +0400

    Add NuGet test project for validating deployment package
    - Include `NullOpsDevs.Libssh.NuGetTest` in solution.
    - Add basic program to test SSH connection, authentication, and command execution.
    - Reference `NullOpsDevs.LibSsh` version `1.0.0-deploy-test`.
    - Target .NET 9.0 framework for NuGet test project.

---
+++ NullOpsDevs.Libssh.NuGetTest/Program.cs
@@ -0,0 +1,8 @@
+﻿using NullOpsDevs.LibSsh.Core;
+using NullOpsDevs.LibSsh.Credentials;
+
+var ssh = new SshSession();
+ssh.Connect("localhost", 2222);
+ssh.Authenticate(SshCredential.FromPassword("user", "12345"));
+
+Console.WriteLine(ssh.ExecuteCommand("ls").Stdout);
